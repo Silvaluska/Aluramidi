971de8f1de1cb7501e15227d8a9678650a16428b
Lucas Silva <lucas030894@gmail.com>
2022-10-10 21:42:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,25 @@ const teclas = document.querySelectorAll('.tecla');
 const audios = document.querySelectorAll('audio');
 
 function tocaSom(id) {
-    audios[id].play();
+    if (id > 9) {
+        alert('Elemento não encontrado');
+    }
+    else if (id < 10) {
+        audios[id].play();
+    }
 }
 
 for (var i = 0; i < 9; i++) {
     const id = i;
     teclas[i].onclick = function () {
         tocaSom(id);
+    }
+    teclas[i].onkeydown = function (evento) {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            teclas[id].classList.add('ativa');
+        }
+    }
+    teclas[i].onkeyup = function () {
+        teclas[id].classList.remove('ativa');
     }
 }
